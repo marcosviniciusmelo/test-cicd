977c7ffbdcfcba648eb1faac5094bc9a3a9005c7
Marcos Vinicius dos Santos Melo <marcos234melo@gmail.com>
2023-02-23 22:06:48 -0300

    fix pipeline test & deploy - 8

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/test", (_req, res) =>  {
+app.get("/api", (_req, res) =>  {
   res.status(200).send("Hello world")
 })
 module.exports = app;
