c5010586b8e4ee5ca115231fbecd016a5b869c20
Marcos Vinicius dos Santos Melo <marcos234melo@gmail.com>
2023-02-23 22:27:39 -0300

    fix pipeline test & deploy - 10

--- src/app.test.js
+++ src/app.test.js
@@ -2,9 +2,9 @@ const app = require("./app")
 const supertest = require("supertest")
 const request = supertest(app)
 
-describe("/test endpoint", () => {
+describe("/api endpoint", () => {
     it("should return a response", async () => {
-        const response = await request.get("/test")
+        const response = await request.get("/api")
         expect(response.status).toBe(200)
         expect(response.text).toBe("Hello world");
     })
